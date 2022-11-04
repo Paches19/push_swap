7093c3f11051f354855bb519e719c74d8e67bfc0
root <root@LAPTOP-UTKVD2O1.localdomain>
2022-11-04 13:19:33 +0100

    check+push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 14:37:59 by adpachec          #+#    #+#             */
-/*   Updated: 2022/11/03 13:36:49 by adpachec         ###   ########.fr       */
+/*   Updated: 2022/11/04 13:01:55 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,33 @@ void	push_swap(t_list *stack_a)
 	min = ft_lstiter(&stack_a, get_min(&stack_a, min));		
 }
 */
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*ptr;
+	size_t			i;
+
+	ptr = (unsigned char *) s;
+	i = -1;
+	while (++i < n && n > 0)
+		ptr[i] = 0;
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void			*ptr;
+	const size_t	len = count * size;
+
+	if (!count)
+		return (malloc(0));
+	if (len / count != size)
+		return (NULL);
+	ptr = (void *) malloc(len);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, len);
+	return (ptr);
+}
+
 void	error_exit(void)
 {
 	write(1, "Error\n", 6);
@@ -73,13 +100,11 @@ int	ft_isdigit(int c)
 	return (1);
 }
 
-int	ft_atoi(char *str)
+long int	ft_atoi(char *str)
 {
-	int		i;
-	int		sign;
-	long	result;
+	int			sign;
+	long int	result;
 
-	i = 0;
 	sign = 1;
 	if (*str == '+' || *str == '-')
 	{
@@ -119,62 +144,120 @@ void	check_num(int argc, char **argv)
 	}
 }
 
-void	check_arg_order(int argc, int *stack)
+void	check_arg_order(int argc, long int **stack)
 {
 	int	i;
 	int	j;
-
+	
 	i = -1;
 	while (++i < argc - 2)
 	{
 		j = i + 1;
-		while (j < argc - 1)
-		{
-			if (stack[i] > stack[j++])
-				return ;
-		}
+		if (stack[0][i] > stack[0][j++])
+			return ;
 	}
+	printf("\nordenado\n");
 	exit (1);
 }
 
-void	check_duplicate_order(int argc, char **argv)
+long int	*check_duplicate_order(int argc, char **argv)
 {
-	int	*stack;
+	int			i;
+	int			j;
+	long int	*stack;
+
+	stack = (long int *)ft_calloc(sizeof(long int), argc);
+	if (!stack)
+		error_exit();
+	stack[argc - 1] = (long int) INT_MAX + 1;
+	i = 0;
+	while (++i < argc)
+	{
+		stack[i - 1] = ft_atoi(argv[i]);
+		j = 0;
+		while (j < i - 1)
+		{
+			if (stack[j++] == stack[i - 1])
+				error_exit();
+		}
+	}
+	check_arg_order(argc, &stack);
+	return (stack);
+}
+
+long int	*check_argv(int argc, char **argv)
+{
+	long int	*stack;
+	
+	check_num(argc, argv);
+	stack = check_duplicate_order(argc, argv);
+	return (stack);
+}
+
+void del_stack(long int ****stack, int pos)
+{
+	while(stack[0][0][0][pos] <= INT_MAX)
+	{
+		stack[0][0][0][pos] = stack[0][0][0][pos + 1];
+		++pos;
+	}
+}
+
+void	sort_positive(long int ***stack_a, long int **stack_b)
+{
 	int	i;
 	int	j;
-	int	k;
+	int	pos;
+	int	unit;
 
 	i = 0;
 	j = 0;
-	stack = (int *)malloc(sizeof(int) * argc);
-	while (++i < argc)
+	pos = 1;
+	unit = 0;
+	while (stack_a[0][0][i] <= INT_MAX)
 	{
-		stack[j] = ft_atoi(argv[i]);
-		k = 0;
-		while (k < j)
+		printf("\nsa: %ld", stack_a[0][0][i]);
+		if(stack_a[0][0][i] > 0)
 		{
-			if (stack[k++] == stack[j])
-				error_exit();
+			printf("\ndiv: %ld", stack_a[0][0][i] / (10 * pos));
+			if(stack_a[0][0][i] / (10 * pos) == unit)
+			{
+				stack_b[0][j] = stack_a[0][0][i];
+				printf("\nsb: %ld", stack_b[0][j]);
+				del_stack(&(stack_a), i);
+			}
 		}
-		++j;
+		++pos;
+		++unit;
+		++i;
 	}
-	check_arg_order(argc, stack);
 }
 
-void	check_argv(int argc, char **argv)
+void	push_swap(int argc, char **argv, long int **stack_a)
 {
-	check_num(argc, argv);
-	check_duplicate_order(argc, argv);
+	long int	*stack_b;
+
+	stack_b = (long int *)ft_calloc(argc, sizeof(long int));
+	if (!stack_b)
+		error_exit();
+	stack_b[argc - 1] = (long int) INT_MAX + 1;
+	if (argv)
+		sort_positive(&(stack_a), &stack_b);
+	free(stack_b);
 }
 
 int	main(int argc, char **argv)
 {
+	long int	*stack_a;
+	
 	if (argc <= 1)
 		exit (-1);
 	else if (argc == 2)
 		return (0);
 	else
-		check_argv(argc, argv);
+		stack_a = check_argv(argc, argv);
 	printf("\nokey\n");
+	push_swap(argc, argv, &stack_a);
+	free(stack_a);
 	return (0);
 }
