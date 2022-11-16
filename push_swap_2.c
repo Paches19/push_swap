cca5a84ea7ba75c028485b14be67c9fed79ad938
root <root@LAPTOP-UTKVD2O1.localdomain>
2022-11-16 12:55:46 +0100

    en teoria funcional

--- push_swap_2.c
+++ push_swap_2.c
@@ -6,7 +6,7 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 14:37:59 by adpachec          #+#    #+#             */
-/*   Updated: 2022/11/15 19:21:01 by adpachec         ###   ########.fr       */
+/*   Updated: 2022/11/16 12:54:51 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -259,6 +259,7 @@ int	rotate_stack_b(long int ***stack)
 	return (1);
 }
 
+//puede q borrar
 int	swap(long int ****stack)
 {
 	long int	temp;
@@ -269,7 +270,7 @@ int	swap(long int ****stack)
 	write(1, "sa\n", 3);
 	return (1);
 }
-
+//puede q borrar
 int	swap_b(long int ***stack)
 {
 	long int	temp;
@@ -331,6 +332,7 @@ int	num_to_push(long int ***stack, int *num_pos, int group_len, int argc)
 	int	i;
 
 	num = INT_MAX;
+	*num_pos = 0;
 	i = -1;
 	while (stack[0][0][++i] <= INT_MAX)
 	{
@@ -392,57 +394,56 @@ bool	check_num_push(long int ***stack, int num)
 	return (true);
 }
 
-void	groups_to_b(int argc, long int ***stack_a, long int **stack_b, int *num_op)
+int	decide_group_len(int argc)
 {
-	int		num;
-	int		st_len;
-	int		num_pos;
-	int		group_len;
-	//int		j;
+	int	group_len;
 
-	st_len = argc - 1;
-	if (argc - 1 <= 200)
+	if (argc - 1 <= 5)
+		group_len = 1;
+	else if (argc - 1 <= 200)
 		group_len = (argc - 1) / 4;
 	else
 		group_len = (argc - 1) / 8;
-	num_pos = 0;
+	return (group_len);
+}
+
+int	get_next_group_len(int argc, int group_len)
+{
+	if (argc - 1 <= 200)
+		group_len += (argc - 1) / 5;
+	else
+		group_len += (argc - 1) / 10;
+	return (group_len);
+}
+
+void	groups_to_b(int argc, long int ***stack_a, long int **stack_b)
+{
+	int		num;
+	int		num_pos;
+	int		group_len;
+
+	group_len = 0;
 	while (!check_end_sort_a(stack_a))
 	{
+		group_len = get_next_group_len(argc, group_len);
 		while (!check_end_group(stack_a, group_len))
 		{
 			num = num_to_push(stack_a, &num_pos, group_len, argc);
 			while (!check_num_push(stack_a, num))
 			{
-				//if (stack_a[0][0][1] < stack_a[0][0][0] && num_pos > st_len / 2)
-					//*num_op += swap(&stack_a);
 				if (stack_a[0][0][0] <= group_len)
-					*num_op += push_b(&stack_a, &stack_b);
+					push_b(&stack_a, &stack_b);
 				else
 				{
-					if (num_pos <= st_len / 2 && stack_a[0][0][1] <= INT_MAX)
-						*num_op += rotate_stack(&stack_a);
+					if (num_pos <= stack_len(&stack_a) / 2 && stack_a[0][0][1] <= INT_MAX)
+						rotate_stack(&stack_a);
 					else if (stack_a[0][0][1] <= INT_MAX)
-						*num_op += reverse_rotate_stack(&stack_a);
+						reverse_rotate_stack(&stack_a);
 				}
 			}
-			st_len = stack_len(&stack_a);
 		}
-		if (argc - 1 <= 200)
-			group_len += (argc - 1) / 4;
-		else
-			group_len += (argc - 1) / 8;
 	}
 	reverse_array_b(&stack_b);
-	/*printf("\n");
-	j = -1;
-	while (stack_b[0][++j] <= INT_MAX)
-		printf("\n sb: %ld", stack_b[0][j]);
-	printf("\n");
-	j = -1;
-	while (stack_a[0][0][++j] <= INT_MAX)
-		printf("\n sa: %ld", stack_a[0][0][j]);
-	printf("\n");*/
-	//exit(1);
 }
 
 long int	*map_stack_a(int argc, long int **stack_a)
@@ -484,12 +485,11 @@ int	get_max_stack(long int ***stack, int *max_pos)
 	return (max);
 }
 
-void	push_to_a(long int ***stack_a, long int **stack_b, int *num_op)
+void	push_to_a(long int ***stack_a, long int **stack_b)
 {
 	int	max;
 	int	max_pos;
 	int	st_len;
-	//int	j;
 
 	max_pos = 0;
 	st_len = 0;
@@ -497,33 +497,25 @@ void	push_to_a(long int ***stack_a, long int **stack_b, int *num_op)
 	{
 		max = get_max_stack(&stack_b, &max_pos);
 		//if (stack_b[0][1] > stack_b[0][0] && max_pos > st_len / 2)
-		//	*num_op += swap_b(&stack_b);
+		//	swap_b(&stack_b);
 		if (stack_b[0][0] == max)
-			*num_op += p_a(&stack_a, &stack_b);
+			p_a(&stack_a, &stack_b);
 		else
 		{
 			if (max_pos <= st_len / 2 && stack_b[0][1] <= INT_MAX)
-				*num_op += rotate_stack_b(&stack_b);
+				rotate_stack_b(&stack_b);
 			else if (stack_b[0][1] <= INT_MAX)
-				*num_op += reverse_rotate_stack_b(&stack_b);
+				reverse_rotate_stack_b(&stack_b);
 		}
 		st_len = stack_len_b(&stack_b);
 	}
-	/*printf("\n");
-	j = -1;
-	while (stack_a[0][0][++j] <= INT_MAX)
-		printf("\nsa: %ld", stack_a[0][0][j]);
-	printf("\n");*/
 }
 
 void	push_swap(int argc, long int **stack_a)
 {
 	long int	*stack_b;
-	int			num_op;
 	int			i;
-	//int			j;
 
-	num_op = 0;
 	stack_b = (long int *)ft_calloc(argc, sizeof(long int));
 	if (!stack_b)
 		error_exit();
@@ -532,14 +524,8 @@ void	push_swap(int argc, long int **stack_a)
 	while (stack_b[++i] <= INT_MAX)
 		stack_b[i] = (long int) INT_MAX + 2;
 	stack_a[0] = map_stack_a(argc, stack_a);
-	groups_to_b(argc, &stack_a, &stack_b, &num_op);
-	push_to_a(&stack_a, &stack_b, &num_op);
-	/*printf("\n");
-	j = -1;
-	while (stack_a[0][++j] <= INT_MAX)
-		printf("\nsa: %ld", stack_a[0][j]);
-	printf("\n");
-	printf("\nnum_op: %d", num_op);*/
+	groups_to_b(argc, &stack_a, &stack_b);
+	push_to_a(&stack_a, &stack_b);
 	free(stack_b);
 	return ;
 }
